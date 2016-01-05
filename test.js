c51bb3d8f8dc0979735b2e387d5907fdf1f3c5bc
Nidhi16 <nidhi.valuable@gmail.com>
2016-01-06 00:16:40 +0530

    ajax teaching

---
+++ test.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('button#one').click(function(){
+	    console.log("hello");
+	});
+	
+	
+    $('button#two').click(function(){
+	    var name = document.myname.n1.val();
+		var email = document.myname.n2.val();
+		var pwd = document.myname.n3.val();
+	});
+});
