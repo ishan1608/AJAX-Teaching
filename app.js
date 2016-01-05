dbc55cb8e6e4b28dc3c3131f6a63ec536a2f1b6b
Ishan <ishanatmuzaffarpur@gmail.com>
2016-01-06 01:32:25 +0530

    Sample of POST and GET requests using jQuery done

--- app.js
+++ app.js
@@ -3,19 +3,34 @@ var http = require('http');
 var nodeStatic = require('node-static');
 var staticServer = new(nodeStatic.Server)();
 var formidable = require('formidable');
+var wurl = require('wurl');
+var url = require('url');
 
 var port = Number(process.env.PORT || 8080);
 
 http.createServer(function(req, res) {
-    console.log(req);
-    if(req.url === '/send') {
-        var form = new formidable.IncomingForm();
-        form.parse(req, function(err, fields, files) {
-            res.writeHead(200, {"Content-Type": "text/plain"});
-            res.write("URL : " + req.url + "\nMETHOD : " + req.method + "\nHEADERS : \n" + JSON.stringify(req.headers));
-            res.end("\nPARAMETERS : \n" + JSON.stringify(fields));
-        });
-    } else {
-        staticServer.serve(req, res);
+    var firstLocation = wurl(1, req.url);
+    console.log(firstLocation);
+    switch(firstLocation) {
+        case 'send':
+            if(req.method === 'POST') {
+                var form = new formidable.IncomingForm();
+                form.parse(req, function(err, fields, files) {
+                    res.writeHead(200, {"Content-Type": "text/plain"});
+                    res.write("URL : " + req.url + "\nMETHOD : " + req.method + "\nHEADERS : \n" + JSON.stringify(req.headers));
+                    res.end("\nPARAMETERS : \n" + JSON.stringify(fields));
+                });
+                break;
+            } else {
+                res.writeHead(200, {"Content-Type": "text/plain"});
+                res.write("URL : " + req.url + "\nMETHOD : " + req.method + "\nHEADERS : \n" + JSON.stringify(req.headers));
+                var url_parts = url.parse(req.url, true);
+                var query = url_parts.query;
+                res.end('\nPARAMETERS : \n' + JSON.stringify(query));
+            }
+        default:
+            staticServer.serve(req, res);
+            break;
     }
 }).listen(port);
+console.log('Server started on port ' + port);
--- test.js
+++ test.js
@@ -1,12 +1,41 @@
 $(document).ready(function(){
     $('button#one').click(function(){
-	    console.log("hello");
+	    $.get("/send").done(function(data) {
+            console.log(data);
+        });
 	});
 	
-	
-    $('button#two').click(function(){
-	    var name = document.myname.n1.val();
-		var email = document.myname.n2.val();
-		var pwd = document.myname.n3.val();
+    $('form').submit(function(event){
+        event.preventDefault();
+	    var name = document.getElementsByName('n1')[0].value;
+		var email = document.getElementsByName('n2')[0].value;
+		var pwd = document.getElementsByName('n3')[0].value;
+        $.post( "/send", { name: name, time: "2pm", email: email, pwd: pwd }).done(function(data) {
+            console.log(data);
+        }).fail(function() {
+            console.log('Request failed');
+        }).always(function() {
+            console.log('Always executed');
+        });
+        $.get( "/send", { name: name, time: "2pm", email: email, pwd: pwd }).done(function(data) {
+            console.log(data);
+        }).fail(function() {
+            console.log('Request failed');
+        }).always(function() {
+            console.log('Always executed');
+        });
+        /*$.post( "/send", { name: name, time: "2pm", email: email, pwd: pwd }, function(data) {
+            console.log(data);
+        });*/
+        /*$.ajax({
+            type: "POST",
+            url: '/send',
+            data: { name: name, time: "2pm", email: email, pwd: pwd },
+            success: function(data) {
+                console.log(data);
+                console.log("Hello");
+            },
+            dataType: 'json'
+        });*/
 	});
 });
